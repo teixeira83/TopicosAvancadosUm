28341fab2dda7cebcc4ebb1e3779511f52472d8f
Rafael Teixeira <rafael.amtx@gmail.com>
2020-11-02 23:00:03 -0300

    Create EqualizarHistograma

--- src/main/java/ExpansaoEEqualizacaoDeHistograma.java
+++ src/main/java/ExpansaoEEqualizacaoDeHistograma.java
@@ -1,8 +1,12 @@
 import ij.IJ;
 import ij.ImagePlus;
-import ij.plugin.Histogram;
 import ij.plugin.PlugIn;
 import ij.process.ImageProcessor;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
 
 public class ExpansaoEEqualizacaoDeHistograma implements PlugIn {
 
@@ -12,6 +16,19 @@ public class ExpansaoEEqualizacaoDeHistograma implements PlugIn {
     int maiorValorPossivel = 255;
     int menorValorImagem = 255;
     int maiorValorImagem = 0;
+    Map<Integer, Integer> dicionarioHistograma = new HashMap<Integer, Integer>();
+    Map<Integer, Integer> dicionarioHistogramaEqualizado = new HashMap<Integer, Integer>();
+    int totalDeElementosPixel = 0;
+
+
+    public int getTotalDeElementosPixel() {
+        return totalDeElementosPixel;
+    }
+
+    public void addTotalDeElementosPixel() {
+        this.totalDeElementosPixel += 1;
+    }
+
 
     public int getMenorValorPossivel() {
         return menorValorPossivel;
@@ -55,22 +72,29 @@ public class ExpansaoEEqualizacaoDeHistograma implements PlugIn {
 
     @Override
     public void run(String s) {
-        IJ.run("Clown");
+        IJ.run("Cell Colony");
         setImagemOriginal(IJ.getImage());
         setProcessadorOriginal(getImagemOriginal().getProcessor());
         IJ.run("Histogram");
+        ImagePlus imagemEmEscalaCinzaExpandir = transformarEmEscalaDeCinzaCalculandoMedia(getProcessadorOriginal());
+        expandirHistograma(imagemEmEscalaCinzaExpandir.getProcessor());
+        imagemEmEscalaCinzaExpandir.show();
+        IJ.run("Histogram");
 
-        ImagePlus imagemEmEscalaCinza = transformarEmEscalaDeCinzaCalculandoMedia(getProcessadorOriginal());
-
-        expandirHistograma(imagemEmEscalaCinza.getProcessor());
-        imagemEmEscalaCinza.show();
+        IJ.run("Nile Bend");
+        setImagemOriginal(IJ.getImage());
+        setProcessadorOriginal(getImagemOriginal().getProcessor());
+        IJ.run("Histogram");
+        ImagePlus imagemEmEscalaCinzaEqualizar = transformarEmEscalaDeCinzaCalculandoMedia(getProcessadorOriginal());
+        equalizarHistograma(imagemEmEscalaCinzaEqualizar.getProcessor());
+        imagemEmEscalaCinzaEqualizar.show();
         IJ.run("Histogram");
     }
 
     private ImagePlus transformarEmEscalaDeCinzaCalculandoMedia(ImageProcessor processadorOriginal){
         int pixel[] = {0, 0, 0};
 
-        ImagePlus imagemEmEscalaCinza = IJ.createImage("Imagem Em Escala de Cinza", "8-bit", processadorOriginal.getWidth(), processadorOriginal.getHeight(), 1);
+        ImagePlus imagemEmEscalaCinza = IJ.createImage("Imagem Em Escala de Cinza", "8-bit", getProcessadorOriginal().getWidth(), getProcessadorOriginal().getHeight(), 1);
 
         for (int i = 0; i < processadorOriginal.getWidth(); i++) {
             for (int j = 0; j < processadorOriginal.getHeight(); j++) {
@@ -121,6 +145,59 @@ public class ExpansaoEEqualizacaoDeHistograma implements PlugIn {
 
                 if (pixel[0] > getMaiorValorImagem()) {
                     setMaiorValorImagem(pixel[0]);
+                }
+            }
+        }
+    }
+
+    private void equalizarHistograma(ImageProcessor processador) {
+        contarIntensidadePixels(processador);
+        calcularValoresPixels(processador);
+        trocarValoresPixels(processador);
+    }
+
+    private void contarIntensidadePixels(ImageProcessor processador) {
+
+        int[] pixel = {0};
+        for (int i = 0; i < processador.getWidth(); i++) {
+            for (int j = 0; j < processador.getHeight(); j++) {
+
+                pixel = processador.getPixel(i, j, pixel);
+
+                if( dicionarioHistograma.get(pixel[0]) != null) {
+                    dicionarioHistograma.put(pixel[0], dicionarioHistograma.get(pixel[0]) + 1);
+                } else {
+                    dicionarioHistograma.put(pixel[0], 1);
+                    addTotalDeElementosPixel();
+                }
+            }
+        }
+    }
+    private void calcularValoresPixels(ImageProcessor processador) {
+
+        Iterator<Integer> numeros = dicionarioHistograma.keySet().iterator();
+
+        float probabilidadeAcumulada = 0;
+        while (numeros.hasNext()) {
+            int intensidade = numeros.next();
+            int quantidadeDePixels = dicionarioHistograma.get(intensidade);
+            float probabilidade = quantidadeDePixels / (float) getTotalDeElementosPixel();
+
+            probabilidadeAcumulada += probabilidade;
+
+            dicionarioHistogramaEqualizado.put(intensidade, (int) ( probabilidadeAcumulada * 0.1 ) );
+        }
+    }
+
+    private void trocarValoresPixels(ImageProcessor processador) {
+        int[] pixel = {0};
+
+        for (int i = 0; i < processador.getWidth(); i++) {
+            for (int j = 0; j < processador.getHeight(); j++) {
+                pixel = processador.getPixel(i, j, pixel);
+
+                if (dicionarioHistogramaEqualizado.get(pixel[0]) != null) {
+                    processador.putPixel(i, j, dicionarioHistogramaEqualizado.get(pixel[0]));
                 }
             }
         }
--- src/main/java/MyImageJApp.java
+++ src/main/java/MyImageJApp.java
@@ -5,9 +5,11 @@ public class MyImageJApp {
 
     public static void main(String[] args) {
         new ImageJ();
-//        IJ.run("Compile and Run...", "compile=/home/z3r0/IdeaProjects/TopicosAvancadosUm/src/main/java/AjustarBrilhoContrasteESolarizacao.java");
-//
         IJ.run("Compile and Run...", "compile=C:/Users/rafael/IdeaProjects/TopicosAvancadosUm/src/main/java/ExpansaoEEqualizacaoDeHistograma.java");
+//        IJ.run("Compile and Run...", "compile=/home/z3r0/IdeaProjects/TopicosAvancadosUm/src/main/java/AjustarBrilhoContrasteESolarizacao.java");
 
     }
+
+
+
 }
